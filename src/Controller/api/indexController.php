9107119891018066effd4dbb2688017de7e9f8ee
Grec Maxime <maxime.grec@gmail.com>
2018-04-27 13:44:31 +0200

    add checkAuth

--- src/Controller/api/indexController.php
+++ src/Controller/api/indexController.php
@@ -63,57 +63,72 @@ class indexController extends Controller
 
     /**
      * @param Req $request
-     * @Route("/refreshToken", methods={"POST"}, name="api_login")
+     * @Route("/refreshToken", methods={"POST"}, name="api_refresh_token")
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
     public function refreshTokenAction(Req $request)
     {
-        $token = $request->get('token');
+        $token          = $request->get('token');
+        $isAuth         = $this->checkAuth($token);
         $em             = $this->getDoctrine()->getManager();
         $repoUser       = $em->getRepository(User::class);
         $rtn            = $repoUser->findOneBy(array('Token' => $token));
 
-        if ($token == null){
-            $data       = array('error' => 'Token is missing');
-        }elseif ($rtn == null){
-            $data       = array('error' => 'Token not match');
-        }else{
+        if ($isAuth == true) {
             $token      = uniqid();
             $rtn->setToken($token);
             $em->persist($rtn);
             $em->flush();
             $data       = array('token' => $token);
         }
+
         return $this->json($data);
     }
 
     /**
      * @param Req $request
-     * @Route("/getLocation", methods={"GET"}, name="api_login")
+     * @Route("/getLocation", methods={"GET"}, name="api_get_location")
      * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
     public function getLocationAction(Req $request)
     {
-        $token = $request->get('token');
+        $token          = $request->get('token');
+        $isAuth         = $this->checkAuth($token);
         $em             = $this->getDoctrine()->getManager();
-        $repoUser       = $em->getRepository(User::class);
         $repoEvent      = $em->getRepository(Event::class);
-        $rtn            = $repoUser->findOneBy(array('Token' => $token));
 
-        if ($token == null){
-            $data       = array('error' => 'Token is missing');
-        }elseif ($rtn == null){
-            $data       = array('error' => 'Token not match');
-        }else{
+        if ($isAuth == true) {
             $date = date('Y-m-d H:i:s');
             $event = $repoEvent->findNextEvent($date);
-
             $data = array(
                 'name' => $event[0]->getName(),
                 'date' => $event[0]->getDate(),
                 'location' => $event[0]->getLocation()->getDescription()
             );
         }
+
         return $this->json($data);
+    }
+
+    /**
+     * @param $token
+     * @return bool|\Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function checkAuth($token)
+    {
+        $em             = $this->getDoctrine()->getManager();
+        $repoUser       = $em->getRepository(User::class);
+        $rtn            = $repoUser->findOneBy(array('Token' => $token));
+
+        if ($token == null){
+            $data       = array('error' => 'Token is missing');
+            echo $this->json($data);
+            die();
+        }elseif($rtn == null){
+            $data       = array('error' => 'Token not match');
+            echo $this->json($data);
+            die();
+        }
+        return true;
     }
 }
